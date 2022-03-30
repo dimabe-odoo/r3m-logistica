9d21ee29739ad544f7df9f080c48e78be311b966
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2022-03-30 12:11:12 -0300

    add vertical align top

--- dimabe_rrhh/models/hr_salary_rule.py
+++ dimabe_rrhh/models/hr_salary_rule.py
@@ -1,4 +1,5 @@
-from odoo import models,fields,api
+from odoo import models, fields, api
+
 
 class HrSalaryRule(models.Model):
     _inherit = 'hr.salary.rule'
@@ -19,16 +20,18 @@ class HrSalaryRule(models.Model):
 
     available_to_settlement = fields.Boolean('Disponible en Finiquito')
 
+    available_to_other_line_ids = fields.Boolean('Disponible en Otros Entradas', default=False)
+
     @api.onchange('is_bonus')
     def onchange_method(self):
-        #if not self.code:
+        # if not self.code:
         #    raise models.UserError('No pude definir un bono sin definir el codigo primero')
         if self.is_bonus:
             self.write({
-                'condition_select':'python',
-                'condition_python':f'result = (inputs.{self.code} and inputs.{self.code}.amount > 0)',
-                'amount_select':'code',
-                'amount_python_compute':f'result = inputs.{self.code}.amount'
+                'condition_select': 'python',
+                'condition_python': f'result = (inputs.{self.code} and inputs.{self.code}.amount > 0)',
+                'amount_select': 'code',
+                'amount_python_compute': f'result = inputs.{self.code}.amount'
             })
 
     @api.onchange('is_permanent_discount')
@@ -50,3 +53,17 @@ class HrSalaryRule(models.Model):
                 'amount_select': 'code',
                 'amount_python_compute': f'result = inputs.{self.code}.amount'
             })
+
+    @api.model
+    def create(self, values):
+        res = super(HrSalaryRule, self).create(values)
+        for r in res:
+            if r.available_to_other_line_ids:
+                input_type = self.env['hr.payslip.input.type'].sudo().search([('code', '=', r.code)], limit=1)
+                if not input_type:
+                    self.env['hr.payslip.input.type'].sudo().create({
+                        'code': r.code,
+                        'name': r.name,
+                        'struct_ids': [(4,r.struct_id.id)]
+                    })
+        return res
